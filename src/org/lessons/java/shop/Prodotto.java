d0d24a6b798f47563ec07a8841ebc451e31a6d90
patrictobola <tobola92@gmail.com>
2023-11-13 15:16:09 +0100

    done all getters

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,9 +2,32 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private int iva;
+	
+	
+		public Prodotto(int codice, String nome, String descrizione, float prezzo, int iva) {
+			
+		}
+		
+		public int getCodice() {
+			return codice;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		
+		public String getDescrizione() {
+			return descrizione;
+		}
+		public float getPrezzo() {
+			return prezzo;
+		}
+		public int getIva() {
+			return iva;
+		}
 }
