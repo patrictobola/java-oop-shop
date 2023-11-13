2eee7191f241f8a1829b0953b87ed282fac6f67b
patrictobola <tobola92@gmail.com>
2023-11-13 15:25:08 +0100

    added setters

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -10,24 +10,50 @@ public class Prodotto {
 	
 	
 		public Prodotto(int codice, String nome, String descrizione, float prezzo, int iva) {
-			
+			setCodice(codice);
+			setNome(nome);
+			setDescrizione(descrizione);
+			setPrezzo(prezzo);
+			setIva(iva);
 		}
 		
+//		Codice
 		public int getCodice() {
 			return codice;
 		}
+		private void setCodice(int codice) {
+			this.codice = codice;
+		}
 		
+//		Nome
 		public String getNome() {
 			return nome;
 		}
-		
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+//		Descrizione
 		public String getDescrizione() {
 			return descrizione;
 		}
+		public void setDescrizione(String descrizione) {
+			this.descrizione = descrizione;
+		}
+		
+//		Prezzo
 		public float getPrezzo() {
 			return prezzo;
 		}
+		public void setPrezzo(float prezzo) {
+			this.prezzo = prezzo;
+		}
+		
+//		Iva
 		public int getIva() {
 			return iva;
+		}
+		public void setIva(int iva) {
+			this.iva = iva;
 		}
 }
