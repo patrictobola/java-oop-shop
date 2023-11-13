46861ea9cebbf7cf70ac373d1935372bb1826e07
patrictobola <tobola92@gmail.com>
2023-11-13 15:47:46 +0100

    did main class and tested code, need minor fixes as negative numbers

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,8 @@
+package org.lessons.java.shop;
+
+public class Main {
+	public static void main(String[] args) {
+		Prodotto p = new Prodotto("Pringles", "Patatine gusto paprika", 2.99f, 20);
+		System.out.println(p);
+	}
+}
